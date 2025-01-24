4ec98d7f7d8a8691f4681a73bdc74f22b3957da4
Carlos Arturo <dev@seekingaura.com>
2025-01-24 13:25:00 +0000

    fix: set expected command magement at entrypoint.sh
    foreman-rake is the default way to call command, but official docker image expects use ruby on rails and the equivalent command is
    
    bundle exec bin/rake

--- docker/prod/entrypoint-with-db-init.sh
+++ docker/prod/entrypoint-with-db-init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-DB_STATUS="$(foreman-rake db:migrate:status 2>&1 | head -n 1)"
+DB_STATUS="$(bundle exec bin/rake db:migrate:status 2>&1 | head -n 1)"
 if [ "$DB_STATUS" == "Schema migrations table does not exist yet."  ]; then
     echo "will init db now"
-    foreman-rake db:migrate RAILS_ENV=production
-    foreman-rake db:seed RAILS_ENV=production
-    foreman-rake permissions:reset RAILS_ENV=production > /var/lib/foreman/.admin-default
+    bundle exec bin/rake db:migrate RAILS_ENV=production
+    bundle exec bin/rake db:seed RAILS_ENV=production
+    bundle exec bin/rake permissions:reset RAILS_ENV=production > /var/lib/foreman/.admin-default
     echo '##################_USERDATA_########################'
     cat /var/lib/foreman/.admin-default
     echo '##################_USERDATA_########################'
